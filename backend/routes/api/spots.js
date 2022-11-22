d51d838737043a0d39b8593f30613fabccb4166d
enkhzayaqt <jini.zaya@gmail.com>
2022-11-22 17:00:56 -0600

    Done get all spots

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+
+// Get all Spots
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll();
+
+    return res.json(spots);
+    }
+);
+
+module.exports = router;
