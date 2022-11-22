5c4a741439b5e0392cad023c98254746d5e2911f
enkhzayaqt <jini.zaya@gmail.com>
2022-11-22 09:24:27 -0600

    options added

--- backend/db/migrations/20221122042016-create-spot.js
+++ backend/db/migrations/20221122042016-create-spot.js
@@ -55,6 +55,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable('Spots', options);
   }
 };
--- backend/db/migrations/20221122042231-create-review-image.js
+++ backend/db/migrations/20221122042231-create-review-image.js
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    await queryInterface.dropTable('ReviewImages', options);
   }
 };
