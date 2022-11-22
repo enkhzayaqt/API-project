44e5c411708e6f1d451d2d0f14b7abcc5256d3f6
enkhzayaqt <jini.zaya@gmail.com>
2022-11-22 11:52:26 -0600

    model file changed

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -15,7 +15,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   SpotImage.init({
-    spotId: DataTypes.INTEGER,
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     url: DataTypes.STRING,
     preview: DataTypes.BOOLEAN
   }, {
