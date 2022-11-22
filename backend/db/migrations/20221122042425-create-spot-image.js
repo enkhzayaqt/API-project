a561051b20a9161e17b4feaa4d3f6f12ed643194
enkhzayaqt <jini.zaya@gmail.com>
2022-11-22 09:35:14 -0600

    options added

--- backend/db/migrations/20221122042425-create-spot-image.js
+++ backend/db/migrations/20221122042425-create-spot-image.js
@@ -34,6 +34,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages', options);
+    await queryInterface.dropTable(options, options);
   }
 };
