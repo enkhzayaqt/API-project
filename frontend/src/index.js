ea0e3ae3520caf321d5d74e66f52233cbb7eab99
enkhzayaqt <jini.zaya@gmail.com>
2023-01-13 15:53:32 -0600

    front-end setup done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 import App from './App';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
