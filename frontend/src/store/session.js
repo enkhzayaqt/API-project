1540edf8385e3ffe6ef22d740fd53bef0a73f8b3
enkhzayaqt <jini.zaya@gmail.com>
2023-01-13 22:22:44 -0600

    login setup done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -27,11 +27,17 @@ export const login = (user) => async (dispatch) => {
         }),
     });
     const data = await response.json();
-    console.log('data:  ', data)
+    // console.log('data:  ', data)
     dispatch(setUser(data));
     return response;
 };
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data));
+    return response;
+}
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
