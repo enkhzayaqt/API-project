c4124ed9ad78b8c5c7767cd54e6b3fb5bab99f45
enkhzayaqt <jini.zaya@gmail.com>
2023-01-14 12:32:16 -0600

    navigation added

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileButton() {
+    return (
+        <div style={{ color: 'blue', fontSize: '100px' }}>
+            <i className="fa-thin fa-circle-user"></i>
+        </div>
+    );
+}
+
+export default ProfileButton;
