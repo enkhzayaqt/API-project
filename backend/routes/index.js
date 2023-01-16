180566d93b39d3b9d98f8b7e561488480429f91f
enkhzayaqt <jini.zaya@gmail.com>
2023-01-15 19:45:42 -0600

    root package.json updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/api/csrf/restore', function (req, res) {
-  const csrfToken = req.csrfToken();
-  res.cookie('XSRF-TOKEN', csrfToken);
-  res.status(200).json({
-    'XSRF-TOKEN': csrfToken
-  })
-});
-
 const apiRouter = require('./api');
+
+// router.get('/api/csrf/restore', function (req, res) {
+//   const csrfToken = req.csrfToken();
+//   res.cookie('XSRF-TOKEN', csrfToken);
+//   res.status(200).json({
+//     'XSRF-TOKEN': csrfToken
+//   })
+// });
+
+// const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
@@ -20,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -31,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -41,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
