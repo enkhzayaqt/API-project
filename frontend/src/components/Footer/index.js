485cd39f523d821ab3b9d84fc8e9838aefef33c4
enkhzayaqt <jini.zaya@gmail.com>
2023-06-07 12:57:46 -0500

    footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,12 +2,14 @@ const Footer = () => {
     return (
         <div id="footer">
             <div>© 2023 Bairbnb</div>
-            <div>Enkhzaya Munkhochir
-            </div>
+
             <div>
             <a href="https://github.com/enkhzayaqt" target="_blank"><i className="fa-brands fa-github"></i></a>
             <a href="https://www.linkedin.com/in/emma-munkhochir/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
             <a href="https://www.facebook.com/emilin.zaya" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+            </div>
+
+            <div>Enkhzaya Munkhochir
             </div>
 
         </div>
